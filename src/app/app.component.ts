29949fe88e14f70e7d3be53a5307161eaeeba417
mxhng <menghuamh@gmail.com>
2023-02-19 16:43:13 -0500

    pre-login navbar disappears when main page is reached

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'project-management-app';
 
-  activeButton = 'Home';
-  active(x: string) {
-    this.activeButton = x;
+  showComponent = true;
+  constructor(private router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showComponent = !router.url.startsWith('/main'); // Replace with your own route
+      }
+    });
   }
 }
