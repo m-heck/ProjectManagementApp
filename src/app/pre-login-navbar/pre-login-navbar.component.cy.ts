af78fc769019e9b9de1518b7bc95664d9c7e3c15
Maren Heck <marenjheck@gmail.com>
2023-02-22 15:01:11 -0500

    made basic cypress test

--- src/app/pre-login-navbar/pre-login-navbar.component.cy.ts
+++ src/app/pre-login-navbar/pre-login-navbar.component.cy.ts
@@ -1,14 +1,19 @@
 
 import { PreLoginNavbarComponent } from './pre-login-navbar.component';
+import { createOutputSpy } from 'cypress/angular'
 
 describe('PreLoginNavbarComponent', () => {
     it('Should display itself and its links', () => {
-        cy.mount(PreLoginNavbarComponent)
+        cy.mount(PreLoginNavbarComponent, {
+//            componentProperties: {
+//                onClick: createOutputSpy('onClickSpy'),
+//            },
+        })
         cy.get('.nav-button-container').contains('Home');
         cy.get('.nav-button-container').contains('Login');
         cy.get('.nav-button-container').contains('Sign Up');
 
-        cy.get('#home').click();
-        cy.get('@onClickSpy').should('have.been.called');
+//        cy.get('#home').click();
+//        cy.get('@onClickSpy').should('have.been.called');
     })
 })
