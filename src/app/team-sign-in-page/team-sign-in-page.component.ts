03de0dcdea50c3de4db894df6df5499a6538d073
mxhng <menghuamh@gmail.com>
2023-02-25 10:15:17 -0500

    fixed login button

--- src/app/team-sign-in-page/team-sign-in-page.component.ts
+++ src/app/team-sign-in-page/team-sign-in-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-team-sign-in-page',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class TeamSignInPageComponent {
 
+  constructor(private _router: Router) { }
+
+  loginButton(): void {
+    this._router.navigate(['/main']);
+  }
+
+  async addUser() {
+  }
 }
