92bc631ea9688efe34342d86cb6552a64037da57
Maren Heck <marenjheck@gmail.com>
2023-03-01 01:08:42 -0500

    new cypress tests

--- src/app/team-sign-in-page/team-sign-in-page.component.cy.ts
+++ src/app/team-sign-in-page/team-sign-in-page.component.cy.ts
@@ -1,15 +1,9 @@
-import { MainPageComponent } from '../main-page/main-page.component';
 import { TeamSignInPageComponent } from './team-sign-in-page.component';
-import { PreLoginNavbarComponent } from '../pre-login-navbar/pre-login-navbar.component';
 
 describe('TeamSignInPageComponent', () => {
-    it('Does the Login Button Work', () => {
-        cy.mount(PreLoginNavbarComponent, {});
-        cy.get('#login');
+    it('Checks that the login button rather than the submit button is displayed', () => {
         cy.mount(TeamSignInPageComponent, {});
-        cy.get('.submit-button').click();
-        cy.wait(150);
-        cy.mount(MainPageComponent, {});
-        cy.get('#title').should('be.visible');
+        cy.get('#loginbutton').contains('Login');
+        cy.get('#loginbutton').contains('Submit').should('not.exist');
     })
 })
