ed12ba97cbf38d98b9a346ab2db891655adfd6e4
Jerry <jerrywang1@ufl.edu>
2023-03-01 00:48:20 -0500

    added more functions

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,8 +17,8 @@ type user struct {
 
 var users = []user{
 	{Username: "gatoralanw", Name: "Alan", Email: "a.wang@ufl.edu", Contact: "3525141846", Password: "IcantactaullyShowmyPasswordLOL"},
-	{Username: "gatoralanw", Name: "Jerry", Email: "j.wang@ufl.edu", Contact: "4076164313", Password: "IcantactaullyShowmyPasswordLOL"},
-	{Username: "gatoralanw", Name: "Max", Email: "m.huang@ufl.edu", Contact: "3523426677", Password: "IcantactaullyShowmyPasswordLOL"},
+	{Username: "TossTheNoodles", Name: "Jerry", Email: "j.wang@ufl.edu", Contact: "4076164313", Password: "IcantactaullyShowmyPasswordLOL"},
+	{Username: "Makshiboi", Name: "Max", Email: "m.huang@ufl.edu", Contact: "3523426677", Password: "IcantactaullyShowmyPasswordLOL"},
 }
 
 func getUsers(context *gin.Context) {
@@ -25,10 +26,44 @@ func getUsers(context *gin.Context) {
 
 }
 
-//func addTodo(context )
+func addUser(context *gin.Context) {
+	var newUser user
+
+	if err := context.BindJSON(&newUser); err != nil {
+		return
+	}
+
+	users = append(users, newUser)
+
+	context.IndentedJSON(http.StatusCreated, newUser)
+}
+
+func getUser(context *gin.Context) {
+	username := context.Param("username")
+	user, err := getUserByUsername(username)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, user)
+}
+
+func getUserByUsername(username string) (*user, error) {
+	for i, t := range users {
+		if t.Username == username {
+			return &users[i], nil
+		}
+	}
+
+	return nil, errors.New("user not found")
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/users", getUsers)
+	router.GET("/users/:username", getUser)
+	router.POST("/users", addUser)
 	router.Run("localhost:3000")
 }
