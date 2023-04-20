ccce6725b76cbb369870c517f892f136c6b1f7ad
Alan Wang <alanwang611@gmail.com>
2023-04-19 21:34:53 -0400

    wrapped up the test cases

--- golang-database/main_test.go
+++ golang-database/main_test.go
@@ -170,3 +170,115 @@ func TestRead {
 }
 
 
+func (d *Driver) Write(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing Collection - no place to save record!")
+	}
+
+
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record (no name)!")
+	}
+
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+
+	dir := filepath.Join(d.dir, collection)
+	fnlPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnlPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return err
+	}
+	b = append(b, byte('\n'))
+
+
+	if err := ioutil.WriteFile(tmpPath, b, 0644); err != nil {
+		return err
+	}
+
+	return os.Rename(tmpPath, fnlPath)
+}
+
+func (d *Driver) Read(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection  - no place to save record!")
+	}
+
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record (no name)!")
+	}
+
+	record := filepath.Join(d.dir, collection, resource)
+
+
+
+	if _, err := os.Stat(record); err != nil {
+		return err
+	}
+
+	b, err := ioutil.ReadFile(record + ".json")
+	if err != nil {
+		return err
+	}
+
+
+	return json.Unmarshal(b, &v)
+}
+
+func (d *Driver) Delete(collection, resource string) error {
+	path := filepath.Join(collection, resource)
+
+	
+	mutex := d.getOrCreateMutex(collection)
+	
+	mutex.Lock()
+	
+	defer mutex.Unlock()
+
+
+	
+	dir := filepath.Join(d.dir, path)
+
+
+	
+	switch fi, err := os.Stat(dir); {
+	case fi == nil, err != nil:
+		return fmt.Errorf("unable to find file or directory named %v\n", path)
+
+	case fi.Mode().IsDir():
+		return os.RemoveAll(dir)
+
+		assert.NoError(t, err)
+	case fi.Mode().IsRegular():
+		return os.RemoveAll(dir + ".json")
+	}
+
+
+	return fmt.Errorf("unexpected error")
+}
+
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
+	
+	d.mutexesLock.Lock()
+	
+	defer d.mutexesLock.Unlock()
+	
+	m, ok := d.mutexes[collection]
+
+	if !ok {
+		m = &sync.Mutex{}
+		d.mutexes[collection] = m
+	}
+
+	return m
+}
